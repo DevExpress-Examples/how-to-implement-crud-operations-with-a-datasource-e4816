19200f0a8899580843ae288532e64df6d172f333
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/app.config.js
@@ -0,0 +1,17 @@
+window.Application1 = $.extend(true, window.Application1, {
+  "config": {
+    "navigationType": "navbar",
+    "navigation": [
+      {
+        "title": "Index",
+        "action": "#Index",
+        "icon": "home"
+      },
+      {
+        "title": "About",
+        "action": "#About",
+        "icon": "info"
+      }
+    ]
+  }
+});
