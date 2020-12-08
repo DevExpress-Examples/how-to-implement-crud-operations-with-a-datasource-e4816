cfdc745e82f82eb75b31961c8dbcecb0fb840147
DevExpressExampleBot <fakeEmail>
2020-12-08 19:36:36 +0300

    Source auto update [skip ci]

---
+++ VB/JS/app.js
@@ -0,0 +1,13 @@
+﻿window.Application1 = window.Application1 || {};
+
+$(function() {
+    app = new DevExpress.framework.html.HtmlApplication({
+        namespace: Application1,
+        navigationType: Application1.config.navigationType,
+        navigation: Application1.config.navigation
+    });
+    app.router.register(':view/:categoryId/:categoryName', { view: 'editView', categoryId: undefined, categoryName: undefined });
+    app.router.register(':view/:id', { view: 'Index', id: undefined });
+    app.navigate('Index');
+});
+
---
+++ VB/JS/views/addView.js
@@ -0,0 +1,15 @@
+﻿Application1.addView = function (params) {
+    var viewModel = {
+        categoryName: ko.observable(),
+        btnSaveClick: function (e) {
+            var category = {
+                CategoryID: 0,
+                CategoryName: viewModel.categoryName()
+            }
+            Application1.db.insert(category).done(function (data) {
+                app.back();
+            });
+        }
+    };
+    return viewModel;
+};
